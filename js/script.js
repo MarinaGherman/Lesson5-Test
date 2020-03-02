4b3172441a060f6b39a4dce309dacd723f9e8e77
Gherman Marina <gherman.marina@icloud.com>
2020-03-02 17:03:17 +0100

    zadanie

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu-item'),
+    column = document.querySelector('.column'),
+    title = document.getElementById('title'),
+    adv = document.querySelector('.adv'),
+    ptpt = document.getElementById('prompt');
+
+
+
+    title.innerText = " Мы продаем только подлинную технику Apple ";
+    menu.replaceChild(menuItem[1], menuItem[2]);
+    menu.insertBefore(menuItem[2], menuItem[1]);
+    adv.remove();
+    const answer = prompt("Do you like Aplle?");
+          ptpt.innerText = answer;
+    document.body.style.background = 'url(img/apple_true.jpg)';
+
